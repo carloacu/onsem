594b6c1862778319e8b73b3ba1bb4ced4c629ffd
carloacu <carloacu.dev@gmail.com>
2022-10-25 15:12:56 +0200

    Add function semanticExpressionCategory_toStr

--- semantictotext/include/onsem/semantictotext/enum/semanticexpressioncategory.hpp
+++ semantictotext/include/onsem/semantictotext/enum/semanticexpressioncategory.hpp
@@ -1,31 +1,51 @@
-#ifndef ONSEM_COMMON_TYPE8ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
-#define ONSEM_COMMON_TYPE8ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
+#ifndef ONSEM_SEMANTICTOTEXT_ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
+#define ONSEM_SEMANTICTOTEXT_ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
 
 #include <string>
+#include <vector>
 
 namespace onsem
 {
 
+#define ONSEM_SEMANTICEXPRESSIONCATEGORY_TABLE                                                    \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(ACTIONDEFINITION, "actionDefinition")                      \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(AFFIRMATION, "affirmation")                                \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(COMMAND, "command")                                        \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(CONDITION, "condition")                                    \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(CONDITIONTOCOMMAND, "conditionToCommand")                  \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(EXTERNALTEACHING, "externalTeaching")                      \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(NOMINALGROUP, "nominalGroup")                              \
+  ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(QUESTION, "question")
 
-#define SEMANTIC_KNOWLEDGECATEGORY_TABLE                                                    \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(ACTIONDEFINITION, "actionDefinition")                      \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(AFFIRMATION, "affirmation")                                \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(COMMAND, "command")                                        \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(CONDITION, "condition")                                    \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(CONDITIONTOCOMMAND, "conditionToCommand")                  \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(EXTERNALTEACHING, "externalTeaching")                      \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(NOMINALGROUP, "nominalGroup")                              \
-  ADD_SEMANTIC_KNOWLEDGECATEGORY(QUESTION, "question")
-
-#define ADD_SEMANTIC_KNOWLEDGECATEGORY(a, b) a,
+#define ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(a, b) a,
 enum class SemanticExpressionCategory
 {
-  SEMANTIC_KNOWLEDGECATEGORY_TABLE
+  ONSEM_SEMANTICEXPRESSIONCATEGORY_TABLE
 };
-#undef ADD_SEMANTIC_KNOWLEDGECATEGORY
+#undef ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY
 
+#define ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY(a, b) b,
+static const std::vector<std::string> _semanticExpressionCategory_toStr = {
+  ONSEM_SEMANTICEXPRESSIONCATEGORY_TABLE
+};
+#undef ADD_ONSEM_SEMANTICEXPRESSIONCATEGORY
+
+#undef ONSEM_SEMANTICEXPRESSIONCATEGORY_TABLE
+
+
+static inline char semanticExpressionCategory_toChar
+(SemanticExpressionCategory pSemanticExpressionCategory)
+{
+  return static_cast<char>(pSemanticExpressionCategory);
+}
+
+static inline std::string semanticExpressionCategory_toStr
+(SemanticExpressionCategory pSemanticExpressionCategory)
+{
+  return _semanticExpressionCategory_toStr[semanticExpressionCategory_toChar(pSemanticExpressionCategory)];
+}
 
 
 } // End of namespace onsem
 
-#endif // ONSEM_COMMON_TYPE8ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
+#endif // ONSEM_SEMANTICTOTEXT_ENUM_SEMANTICEXPRESSIONCATEGORY_HPP
