347a19835c5fd2f77c39f62a7aef6bf81beae5ca
carloacu <carloacu.dev@gmail.com>
2023-02-09 13:40:23 +0100

    Robustify for usage by another repository

--- onsem-config.cmake
+++ onsem-config.cmake
@@ -1,6 +1,7 @@
 get_filename_component(_onsem_root "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(_onsem_root "${_onsem_root}" ABSOLUTE)
 
+include(${_onsem_root}/subdirectories/boost/boost_property_tree_with_deps.cmake)
 include(${_onsem_root}/common/onsemcommon-config.cmake)
 include(${_onsem_root}/typeallocatorandserializer/onsemtypeallocatorandserialyzer-config.cmake)
 include(${_onsem_root}/compilermodel/onsemcompilermodel-config.cmake)
