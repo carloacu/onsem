4a2a3b13f926ad28d66f86619c1daed87ec705d4
carloacu <carloacu.dev@gmail.com>
2023-02-16 16:21:35 +0100

    Consider that "us" refers to humans

--- tests/onsemgtests/src/semantictotext/triggers/triggers_match.cpp
+++ tests/onsemgtests/src/semantictotext/triggers/triggers_match.cpp
@@ -168,6 +168,7 @@ TEST_F(SemanticReasonerGTests, operator_reactFromTrigger_basic)
   ONSEM_BEHAVIOR_EQ(reaction10, triggers_match("Descends ton volume", semMem, lingDb, SemanticLanguageEnum::FRENCH));
   ONSEM_ANSWER_EQ(reaction11, triggers_match(trigger11, semMem, lingDb));
   ONSEM_ANSWER_EQ(reaction11, triggers_match("tell me who you are", semMem, lingDb));
+  ONSEM_ANSWER_EQ(reaction11, triggers_match("tell us who you are", semMem, lingDb));
   ONSEM_ANSWER_EQ(reaction11, triggers_match("please tell me who you are", semMem, lingDb));
 }
 
