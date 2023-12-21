37e9a9ada4d55c2c2f339178b2075e75a2cf50d4
carloacu <carloacu.dev@gmail.com>
2023-12-21 13:24:48 +0100

    Fix turnAway concept

--- tests/onsemgtests/src/semantictotext/triggers/triggers_match.cpp
+++ tests/onsemgtests/src/semantictotext/triggers/triggers_match.cpp
@@ -130,6 +130,8 @@ TEST_F(SemanticReasonerGTests, operator_reactFromTrigger_basic_fr)
   const std::string reaction21 = "Il est 18 heures.";
   const std::string trigger22 = "Dis la date";
   const std::string reaction22 = "Il est le 6 janvier.";
+  const std::string trigger23 = "retourne à ta maison"; // Vas is wrongly written in purpose for the test
+  const std::string reaction23 = "Je me rechargerai.";
 
   ONSEM_NOANSWER(triggers_match(whoAreYou, semMem, lingDb));
   ONSEM_NOANSWER(triggers_match(stopApplication, semMem, lingDb));
@@ -172,6 +174,7 @@ TEST_F(SemanticReasonerGTests, operator_reactFromTrigger_basic_fr)
   triggers_add(trigger20, reaction20, semMem, lingDb);
   triggers_add(trigger21, reaction21, semMem, lingDb);
   triggers_add(trigger22, reaction22, semMem, lingDb);
+  triggers_add(trigger23, reaction23, semMem, lingDb);
 
 
   ONSEM_ANSWER_EQ(iAmYourFrined, triggers_match(whoAreYou, semMem, lingDb));
@@ -230,6 +233,8 @@ TEST_F(SemanticReasonerGTests, operator_reactFromTrigger_basic_fr)
   ONSEM_BEHAVIOR_EQ(reaction21, triggers_match(trigger21, semMem, lingDb));
   ONSEM_NOANSWER(triggers_match("parle moins fort", semMem, lingDb));
   ONSEM_BEHAVIOR_EQ(reaction22, triggers_match(trigger22, semMem, lingDb));
+  ONSEM_BEHAVIOR_EQ(reaction23, triggers_match(trigger23, semMem, lingDb));
+  ONSEM_NOANSWER(triggers_match("retourne toi", semMem, lingDb));
 }
 
 TEST_F(SemanticReasonerGTests, operator_reactFromTrigger_basic_en)
